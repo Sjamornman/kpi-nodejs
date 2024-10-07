ef51941e0ab0ba59c85b702b8e2bed883c10beb3
Sjamornman <sittisak.jamornman@gmail.com>
2024-10-07 11:33:30 +0700

    Update testapi.js

--- testapi.js
+++ testapi.js
@@ -1,20 +1,29 @@
-const bodyOarser = require("body-parser");
+const bodyParser = require("body-parser");
 const express = require("express");
 const cors = require("cors");
+const https = require("https");
+const fs = require("fs");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(bodyOarser.json(), cors());
+// Load SSL certificate and key
+const options = {
+  key: fs.readFileSync("private-key.pem"), 
+  cert: fs.readFileSync("certificate.pem"), 
+};
+
+app.use(bodyParser.json(), cors());
 
 app.get("/", (req, res) => {
-  res.send("welcome to test api");
+  res.send("Welcome to the test API");
 });
 
 app.get("/api", (req, res) => {
   res.json([{ count_a: 7, count_b: 2 }]);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
+// Create HTTPS server
+https.createServer(options, app).listen(PORT, () => {
+  console.log(`HTTPS Server is running on port ${PORT}`);
 });
