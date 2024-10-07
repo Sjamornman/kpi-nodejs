a7f4f433953f435186c82295688f4e7e0b65699d
Sjamornman <sittisak.jamornman@gmail.com>
2024-10-07 10:34:40 +0700

    Add files via upload

---
+++ testapi/testapi.js
@@ -0,0 +1,20 @@
+const bodyOarser = require("body-parser");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(bodyOarser.json(), cors());
+
+app.get("/", (req, res) => {
+  res.send("welcome to test api");
+});
+
+app.post("/api", (req, res) => {
+  res.json([{ count_a: 7, count_b: 2 }]);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
